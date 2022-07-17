68f48477923bd4f4ca0d4607501d5e2869f13b54
粵典注得了 <info+bpmfwordle@words.hk>
2022-07-17 16:57:17 +0800

    Minor tweaks to keyboard layout

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -65,7 +65,9 @@ export const Keyboard = ({
             isRevealing={isRevealing}
           />
         ))}
-        <span style={{width:42}}> </span>
+        <Key width={36} value="DELETE" onClick={onClick}>
+         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"> <path fill="var(--color-tone-1)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path> </svg>
+        </Key>
       </div>
       <div className="flex justify-center mb-1">
         <span style={{width:12}}> </span>
@@ -94,9 +96,7 @@ export const Keyboard = ({
         <span style={{width:38}}> </span>
       </div>
       <div className="flex justify-center">
-        <Key width={36} value="DELETE" onClick={onClick}>
-         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"> <path fill="var(--color-tone-1)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path> </svg>
-        </Key>
+        <span style={{width:40}}> </span>
         {['ㄈ', 'ㄌ', 'ㄏ', 'ㄒ', 'ㄖ', 'ㄙ', 'ㄩ', 'ㄝ','ㄡ', 'ㄥ'].map((key) => (
           <Key
             value={key}
